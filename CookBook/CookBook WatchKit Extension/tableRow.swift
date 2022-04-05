bb34c98b2de63eee6eb58c758e61c17358dfcafa
AlekseevKA <92131849+AlekseevKA@users.noreply.github.com>
2022-04-05 14:19:00 +0900

    Add files via upload

---
+++ CookBook/CookBook WatchKit Extension/tableRow.swift
@@ -0,0 +1,14 @@
+//
+//  tableRow.swift
+//  CookBook
+//
+//  Created by Алексеев on 04.04.2022.
+//
+
+import WatchKit
+
+class tableRow: NSObject {
+    @IBOutlet weak var RecipeIconLabel: WKInterfaceLabel!
+    
+    @IBOutlet weak var RecipeNameLabel: WKInterfaceLabel!
+}
